94bbe96697d2f86d25adcab0c577f10ddb07ae1c
rodrigo <rodrigo@localhost>
2020-07-17 23:28:11 -0300

    .

--- src/main/resources/static/assets/js/load.js
+++ src/main/resources/static/assets/js/load.js
@@ -1,11 +1,11 @@
+var Swagger = function(nome, descricao, json) {
+	this.nome = nome;
+	this.descricao = descricao;
+	this.json = json;
+}
+
 $(document).ready(function() {
 	
-	var Swagger = function(nome, descricao, json) {
-		this.nome = nome;
-		this.descricao = descricao;
-		this.json = json;
-	}
-
 	var reg1 = new Swagger("Petstore 1", "API Petstore 1", "https://petstore.swagger.io/v2/swagger.json");
 	var reg2 = new Swagger("Petstore 2", "API Petstore 2", "https://petstore.swagger.io/v2/swagger.json");
 	var reg3 = new Swagger("Petstore 3", "API Petstore 3", "https://petstore.swagger.io/v2/swagger.json");
@@ -21,8 +21,8 @@ $(document).ready(function() {
 							  + '  <td>'+ swaggers[i].nome +'</td>'
 							  + '  <td>'+ swaggers[i].descricao +'</td>'
 							  + '  <td><a href="'+ swaggers[i].json +'">'+ swaggers[i].json +'</a></td>'
-							  + '  <td width="120px"><a class="btn btn-swagger btn-block btn-sm" href="/swagger-ui/V2.2.8/index.html?json=https://petstore.swagger.io/v2/swagger.json"><i class="fas fa-book-open mr-2"></i>2.2.8</a></td>'
-							  + '  <td width="120px"><a class="btn btn-swagger btn-block btn-sm" href="/swagger-ui/V3.0.3/index.html?json=https://petstore.swagger.io/v2/swagger.json"><i class="fas fa-book-open mr-2"></i>3.0.3</a></td>'							
+							  + '  <td width="120px"><a class="btn btn-swagger btn-block btn-sm" href="/swagger-ui/V2.2.8/index.html?json='+ swaggers[i].json +'"><i class="fas fa-book-open mr-2"></i>2.2.8</a></td>'
+							  + '  <td width="120px"><a class="btn btn-swagger btn-block btn-sm" href="/swagger-ui/V3.0.3/index.html?json='+ swaggers[i].json +'"><i class="fas fa-book-open mr-2"></i>3.0.3</a></td>'							
 						      + '</tr>');	
 	}
 	
